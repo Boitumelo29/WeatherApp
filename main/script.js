a495fbcb7298c0c297f984ee89c326e593904bc3
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-03-15 20:39:24 +0200

    update

---
+++ main/script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.serach-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-deatils');
+
+
+search.addEventListener('click', ()=>{
+
+    const APIKey = '206bf7b428a6f63ded997e4f300d0d04';
+    const city = document.querySelector('search input').value;
+
+    ///13:06
+});
